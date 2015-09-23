f9dc406fc624c2d2771075821bf7da9d690169c2
Shirshendu Mukherjee <shirshendu.mukherjee.88@gmail.com>
2015-09-24 03:58:16 +0530

    Add authorization by setting EUID to current user, and escalating it back at the end of request

--- app/controllers/navigation_controller.rb
+++ app/controllers/navigation_controller.rb
@@ -1,6 +1,16 @@
 class NavigationController < ApplicationController
+  before_filter :set_euid_to_current_user
+  after_filter :reset_euid
+
   def show
     @path = params[:fspath]
 
+  private
+  def set_euid_to_current_user
+    Process.euid = Etc.getpwnam(current_username).uid
+  end
+
+  def reset_euid
+    Process.euid = Process.uid
   end
 end
