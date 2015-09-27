62e9d157e098234961389e21c86018f3074d5151
Shirshendu Mukherjee <shirshendu.mukherjee.88@gmail.com>
2015-09-27 10:05:52 +0530

    Add error handling route and root login config

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -2,8 +2,18 @@ module Authorization
   extend ActiveSupport::Concern
 
   included do
+    before_filter :check_login
     before_filter :set_euid_to_current_user
     after_filter :reset_euid
+    rescue_from FilesystemItem::Error, SystemCallError, with: :error
+  end
+
+  def user_signed_in?
+    session[:username]
+  end
+
+  def check_login
+    redirect_to login_path, flash: { error: 'You are not logged in' } unless user_signed_in?
   end
 
   def set_euid_to_current_user
@@ -11,6 +21,12 @@ module Authorization
   end
 
   def reset_euid
+    puts 'euid reset'
     Process.euid = Process.uid
+  end
+
+  def error(e)
+    reset_euid
+    redirect_to file_path(path: Dir.home), flash: { error: e.message }
   end
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,15 +2,20 @@ require 'rpam'
 
 class LoginController < ApplicationController
   def new
+    redirect_to file_path if current_username
   end
 
   def create
     credentials = params[:login]
-    @success = Rpam.auth(credentials[:username], credentials[:password])
+    if credentials[:username] == 'root' && !Rails.configuration.allow_root_login
+      @success = false
+    else
+      @success = Rpam.auth(credentials[:username], credentials[:password])
+    end
     if @success
       session[:username] = credentials[:username]
       flash[:success] = 'Welcome to the FileSystem Rover'
-      redirect_to navigation_path path: Dir.home
+      redirect_to file_path(path: Dir.home)
     else
       flash[:error] = 'Login failed'
       redirect_to login_path
